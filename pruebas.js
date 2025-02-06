0c1cd0e8a500dee95eec8832d5d51a7301f13b97
Martin Bernal <78180863+MartinBernal06@users.noreply.github.com>
2025-02-06 09:33:31 -0700

    Caracteristica 4 lista

--- pruebas.js
+++ pruebas.js
@@ -1,3 +1,4 @@
 console.log('Hola mundo, ¡Estoy trabajando con control de versiones git!')
 console.log('Esta es una caracteristica')
 console.log('Esta es una caracteristica2')
+console.log('Esta es una caracteristica4')
